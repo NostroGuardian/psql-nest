7126044d0890556a13dda4c263cec705907a8ce9
nostroguardian <shmelivan5@yandex.ru>
2025-01-19 14:29:55 +0300

    postgres config created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getPostgresConfig } from './configs/postgres.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getPostgresConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
